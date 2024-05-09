0ce2aedf5604b58470b279272f816ce97dbb966a
sofefaal@gmail.com <sofefaal@gmail.com>
2024-05-09 09:00:50 +0100

    added a sign in functionality, task 7 in progress

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,23 @@
-import { Routes, Route } from 'react-router-dom'
-import Home from './Components/Home'
-import Header from './Components/Header'
-import './App.css'
-import SingleArticle from './Components/SingleArticle'
+import { Routes, Route } from "react-router-dom";
+import Home from "./Components/Home";
+import Header from "./Components/Header";
+import "./App.css";
+import SingleArticle from "./Components/SingleArticle";
+import { UserProvider } from "./context/UserContext";
+
 
 function App() {
-  
-
-  return(
+  return (
     <div>
-      <Header />
-      <Routes>
-        <Route path="/" element={<Home/>} />
-        <Route path="/articles/:article_id" element={<SingleArticle />} />
-      </Routes>
+      <UserProvider>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/articles/:article_id" element={<SingleArticle />} />
+        </Routes>
+      </UserProvider>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/Components/SignInUser.jsx
+++ src/Components/SignInUser.jsx
@@ -1,7 +1,48 @@
-
+import { useState, useEffect, useContext } from "react";
+import { getAllUsers } from "../api";
+import { UserContext } from "../context/UserContext";
+import User from "./icons/User";
 
 function SignInUser() {
+  const [selectedUser, setSelectedUsers] = useState(null);
+  const { userLogin, setUserLogin } = useContext(UserContext);
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
 
+  const handleSelectUser = () => {
+    if (!selectedUser) return
+    setUserLogin(selectedUser);
+    setIsLoggedIn(true);
+  };
+  
+  useEffect(() => {
+      getAllUsers().then((response) => {
+          setSelectedUsers(response.data.users[3]);
+        });
+
+    }, []);
+    
+  console.log(selectedUser);
+  return (
+    <section>
+      <button onClick={handleSelectUser} disabled={isLoggedIn}>
+        {isLoggedIn ? (
+          <>
+            <img
+              src={selectedUser?.avatar_url}
+              alt="user avatar character"
+              className="user-avatar"
+            />
+            <div className="username">{selectedUser?.username}</div>
+          </>
+        ) : (
+          <>
+            <User />
+            <span>Sign In</span>
+          </>
+        )}
+      </button>
+    </section>
+  );
 }
 
-export default SignInUser
+export default SignInUser;
